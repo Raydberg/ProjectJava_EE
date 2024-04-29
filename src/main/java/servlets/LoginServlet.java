e5aedb4945f488e2e89edde0aac36e1adb6eec4e
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-04-28 19:24:28 -0500

    LoginServlet

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -1,41 +1,67 @@
 package servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginServlet
- */
+import bd.SqlConexion;
+import modelo.Trabajador;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+    public LoginServlet() { super(); }
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String nombre = request.getParameter("nombre");
+		String contrasenia = request.getParameter("contrasenia");
+		
+		
+		try  (  				
+				Connection conexion = SqlConexion.obtenerConexion();
+				PreparedStatement ps = conexion.prepareStatement("SELECT * FROM Trabajadores WHERE nombre = ? AND contrasenia = ?");
+		)
+		{
+			ps.setString(1, nombre);
+			ps.setString(2, contrasenia);
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+			Trabajador trabajador = new Trabajador(
+					rs.getInt("id"),
+                    rs.getString("dni"),
+                    rs.getString("apellido"),
+                    rs.getString("nombre"),
+                    rs.getString("contrasenia"),
+                    rs.getDouble("salario"),
+                    rs.getString("email"),
+                    rs.getString("direccion"),
+                    rs.getString("genero"),
+                    rs.getString("fechaNacimiento"),
+                    rs.getInt("idPuesto"),
+                    rs.getString("telefono")
+					
+					);
+			request.getSession().setAttribute("trabajador", trabajador);
+			response.sendRedirect("/JSP/Login.jsp");
+			
+			}else{
+				 response.sendRedirect("ReguisterUser.html");
+			}
+			
+			
+		} catch (SQLException e) {
+			System.out.println("Error en la consulta");
+			e.printStackTrace();
+		}
+		
 	}
 
 }
