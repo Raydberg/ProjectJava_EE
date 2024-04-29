626b8b4f48c5452255ca0c62f6c3207589067e2c
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-04-28 19:00:09 -0500

    Cambios

--- src/main/java/servlets/emp/AdminServlet.java
+++ src/main/java/servlets/emp/AdminServlet.java
@@ -1,4 +1,4 @@
-package servlets;
+package servlets.emp;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -7,32 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Hola
- */
-@WebServlet("/Hola")
-public class Hola extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Hola() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+@WebServlet("/AdminServlet")
+public class AdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public AdminServlet() { super(); }
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
